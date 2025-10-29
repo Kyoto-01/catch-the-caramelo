31a4b7f5bfa8d615ded1ff48944ed926211ca05a
Kyoto-01 <cameraman269@gmail.com>
2025-10-29 18:26:18 -0300

    FIX: Transformando areiavermelha.txt em um endpoint

--- app.py
+++ app.py
@@ -4,9 +4,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/static/areiavermelha.txt")
+def areia_vermelha_flag():
+    return """
+
+
+             /\_/\  
+           =( °w° )=   < AUAU..err..MIAU você não vai me parar!!
+             )   (  //
+            (__ __)//
+
+
+FLAG{caramelo-filho-da-mae}
+
+"""
 
 
 if __name__ == "__main__":
