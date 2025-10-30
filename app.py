31bcd4b3e616567e53c0361981bc63d16686406a
Kyoto-01 <cameraman269@gmail.com>
2025-10-30 11:36:17 -0300

    ADD: Endpoints das flags

--- app.py
+++ app.py
@@ -1,23 +1,81 @@
-from flask import Flask
+"""
+
+formato da flag: { "flag": "FLAG{.....}" }
+
+"""
+
+
+from flask import (
+  Flask, 
+  request
+)
 
 
 app = Flask(__name__)
 
 
-@app.route("/static/areiavermelha.txt")
+def check_flag(
+  request,
+  flagOriginal: str
+):
+
+  try:
+      
+    flagUser = request.get_json()
+    flagUser = flagUser.get("flag")
+  
+  except:
+     
+    "Bad Request", 400
+
+  if flagUser == flagOriginal:
+      
+      return "OK", 200
+
+  else:
+      
+      return "Unauthorized", 401 
+
+
+@app.post("/flag1")
 def areia_vermelha_flag():
-    return """
+    flagOriginal = "FLAG{miau-miau-sou-muito-mau}"
+    return check_flag(request, flagOriginal)
 
 
-             /\_/\  
-           =( °w° )=   < AUAU..err..MIAU você não vai me parar!!
-             )   (  //
-            (__ __)//
+@app.post("/flag2")
+def areia_vermelha_flag():
+    flagOriginal = "FLAG{miawlware-melhor-que-caramelo}"
+    return check_flag(request, flagOriginal)
 
 
-FLAG{caramelo-filho-da-mae}
+@app.post("/flag3")
+def areia_vermelha_flag():
+    flagOriginal = "FLAG{caramelo-filho-da-mae}"
+    return check_flag(request, flagOriginal)
 
-"""
+
+@app.post("/flag4")
+def areia_vermelha_flag():
+    flagOriginal = "FLAG{voce-me-paga-carameloooo}"
+    return check_flag(request, flagOriginal)
+
+
+@app.post("/flag5")
+def areia_vermelha_flag():
+    flagOriginal = "FLAG{hehehe-estou-ficando-mais-forte}"
+    return check_flag(request, flagOriginal)
+
+@app.post("/flag6")
+def areia_vermelha_flag():
+    flagOriginal = "FLAG{ta-de-sacanagem-caramelo-slk-kkkk}"
+    return check_flag(request, flagOriginal)
+
+
+@app.post("/flag7")
+def areia_vermelha_flag():
+    flagOriginal = "FLAG{eh-o-meu-fim-ninguem-derrota-o-caramelo}"
+    return check_flag(request, flagOriginal)
 
 
 if __name__ == "__main__":
